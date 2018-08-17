2d665cde51531dbb4b5b5b982506866b57d8ab25
timkeane <tkeane@doitt.nyc.gov>
2018-08-16 21:48:56 -0400

    .

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const proj4 = require('./src/proj4')
 const postgis = require('./src/postgis')
 
 const app = express()
-const port = process.env.PORT || 3000
 
 app.get('/proj4/:fromEpsg/:toEpsg/:x/:y/', proj4.transform)
 app.get('/postgis/:fromEpsg/:toEpsg/:x/:y/', postgis.transform)
 
-module.exports = app.listen(port)
+module.exports = app.listen(process.env.PORT || 3000)
