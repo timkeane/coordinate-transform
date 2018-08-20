d88f9d44e712ffcf836b260583f44dba3a978193
timkeane <tkeane@doitt.nyc.gov>
2018-08-20 11:08:40 -0400

    postgis integration test

---
+++ __tests__/postgis.integration.test.js
@@ -0,0 +1,14 @@
+require('isomorphic-fetch')
+
+const server = require('../index')
+
+test('proj4 integration test', done => {
+  fetch('http://localhost:3000/postgis/EPSG:2263/EPSG:4326/988217/192020')
+    .then(respose => {
+      return respose.json()
+    }).then(coordinate => {
+      expect(coordinate[0]).toBe(-73.9856945509795)
+      expect(coordinate[1]).toBe(40.69372638702077)
+      server.close(done)
+    })
+})
