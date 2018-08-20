3fc4fff93885a312c5a9331ff6f19f8a54dea4f0
timkeane <tkeane@doitt.nyc.gov>
2018-08-20 16:42:19 -0400

    better tests

--- __tests__/postgis.integration.test.js
+++ __tests__/postgis.integration.test.js
@@ -9,12 +9,15 @@ afterEach(() => {
 })
 
 test('proj4 integration test', done => {
+  expect.assertions(2)
   fetch('http://localhost:3000/postgis/EPSG:2263/EPSG:4326/988217/192020')
     .then(respose => {
       return respose.json()
     }).then(coordinate => {
       expect(coordinate[0]).toBe(-73.9856945509795)
       expect(coordinate[1]).toBe(40.6937263870208)
+      server.close(done)
+    }).catch(() => {
       server.close(done)
     })
 })
