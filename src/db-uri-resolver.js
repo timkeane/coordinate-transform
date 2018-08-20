aac6a78b4b15e8b61d3b64a4ad377478cea11338
timkeane <tkeane@doitt.nyc.gov>
2018-08-20 14:13:07 -0400

    temporary logging

--- src/db-uri-resolver.js
+++ src/db-uri-resolver.js
@@ -1,5 +1,11 @@
 let dbUri = process.env.VCAP_SERVICES 
+
+console.log('process.env.VCAP_SERVICES=', process.env.VCAP_SERVICES)
+
 if (dbUri) {
+
+  console.log('Parsed', JSON.parse(dbUri))
+
   dbUri = JSON.parse(dbUri)['user-provided'].credentials.uri
 } else {
   dbUri = process.env.POSTGIS_URI
